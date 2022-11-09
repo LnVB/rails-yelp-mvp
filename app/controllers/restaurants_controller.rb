e9456a0517637af4f5fe6f18b59c76da0208332b
Helene Buffet <hebuffet@gmail.com>
2022-11-09 16:49:13 -0500

    without improvement

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: %i[show]
+  before_action :set_restaurant, only: :show
 
   # GET /restaurants
   def index
@@ -27,6 +27,7 @@ class RestaurantsController < ApplicationController
   end
 
   private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
